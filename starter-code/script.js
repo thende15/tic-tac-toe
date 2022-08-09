a9803155bd882e7583e87f5a2d91fc8c4040141e
Timothy Henderson <titansmith8@hotmail.com>
2022-08-09 14:29:04 -0400

    got board click sorted

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,12 +1,17 @@
 let vsPlayer = document.getElementById("new_v_play")
-const ex = document.getElementsByClassName("ex")
-const oh = document.getElementsByClassName("oh")
+// const ex = document.getElementsByClassName("ex")
+// const oh = document.getElementsByClassName("oh")
 
-vsPlayer = addEventListener(onclick, setPlayerOne)
+vsPlayer.addEventListener('click', setPlayerOne)
 function setPlayerOne() {
-    let playerOne = document.getElementsByName("player_select").values()
+    let playerOne = document.getElementsByName("player_select");
     console.log(playerOne)
 }
 
-let board = document.getElementsByClassName("game_space")
-board.click(console.log("clicked!"))
+let board = document.querySelector(".game_space");
+board.addEventListener('click', setPiece);
+function setPiece(e) {
+    if (e.target.className === "block") {
+        console.log("piece set!")
+    }
+}
