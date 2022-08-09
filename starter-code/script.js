896a0c17333eb05963dfea6fc2ddf064bc2edc77
Timothy Henderson <titansmith8@hotmail.com>
2022-08-09 13:26:16 -0400

    added a few variables

---
+++ starter-code/script.js
@@ -0,0 +1,9 @@
+let vsPlayer = document.getElementById("new_v_play")
+const ex = document.getElementsByClassName("ex")
+const oh = document.getElementsByClassName("oh")
+
+vsPlayer.click(); {
+    let player1 = document.getElementsByName("player_select").values()
+    console.log(player1)
+    
+}
