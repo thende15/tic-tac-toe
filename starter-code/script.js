16148f76bba5d8c3c7523be0788efdcf03531c48
Timothy Henderson <titansmith8@hotmail.com>
2022-08-10 13:59:06 -0400

    piddling with svg

--- starter-code/script.js
+++ starter-code/script.js
@@ -22,13 +22,27 @@ function setPlayerOne() {
 //     if (e.target.className === "block") {
 //         console.log("piece set!")
 //     }
-// }
+// } 
+/////////////////
+
+// let svg = document.getElementById("test"); //Get svg element
+
+
+// let newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
+
+// newElement.setAttribute("d","M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z"); //Set path's data
+
+// newElement.style.fill = "#979797"
+// newElement.style.fillRule = "evenodd"
+// svg.appendChild(newElement);
+
 let cells = document.querySelectorAll(".game_space div"), turns = 0;
 for (let i = 0; i< cells.length; i++) {
     cells[i].onclick = function() {
         if (this.innerHTML !== "X" && this.innerHTML !== "O") {
             if (turns % 2 === 0) {
                 this.innerHTML = "X";
+                //turn.prepend()
                 turns += 1;
             } else {
                 if (turns % 2 === 1) {
@@ -71,16 +85,7 @@ for (let i = 0; i< cells.length; i++) {
 // }
 
 
-// var svg = document.getElementById("1"); //Get svg element
 
-
-// var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
-
-// newElement.setAttribute("d","M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z"); //Set path's data
-
-// newElement.style.fill = "#979797"
-// newElement.style.fillRule = "evenodd"
-// svg.appendChild(newElement);
 
 
 //Edit this (Replay)
