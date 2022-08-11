fb6dc5433e658a51fd5c0da01857a11c9fb988e6
Timothy Henderson <titansmith8@hotmail.com>
2022-08-11 12:59:38 -0400

    chanigng font size

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,6 +1,5 @@
 let vsPlayer = document.getElementById("new_v_play")
-// const ex = document.getElementsByClassName("ex")
-// const oh = document.getElementsByClassName("oh")
+
 
 vsPlayer.addEventListener('click', setPlayerOne)
 function setPlayerOne() {
@@ -25,29 +24,22 @@ function setPlayerOne() {
 // } 
 /////////////////
 
-// let svg = document.getElementById("test"); //Get svg element
 
-
-// let newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
-
-// newElement.setAttribute("d","M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z"); //Set path's data
-
-// newElement.style.fill = "#979797"
-// newElement.style.fillRule = "evenodd"
-// svg.appendChild(newElement);
 
 let cells = document.querySelectorAll(".game_space div"), turns = 0;
 for (let i = 0; i < cells.length; i++) {
     cells[i].onclick = function() {
         if (this.innerHTML !== "&#215;" && this.innerHTML !== "&#8858;") {
-            if (turns % 2 === 0) {
+            if (turns % 2 === 0 && this.innerHTML !== "&#8858;") {
                 this.innerHTML = "&#215;";
-                turn.innerHTML = "&#8858;'s turn"
+                turn.innerHTML = "&#8858;'s turn";
+                winCon();
                 turns += 1;
             } else {
-                if (turns % 2 === 1) {
+                if (turns % 2 === 1 && this.innerHTML !== "&#215;") {
                     this.innerHTML = "&#8858;";
-                    turn.innerHTML = "&#215;'s turn"
+                    turn.innerHTML = "&#215;'s turn";
+                    winCon();
                     turns += 1;
             }
         }
@@ -60,6 +52,7 @@ function winner(c1, c2, c3) {
     c2.classList.add("win");
     c3.classList.add("win");
     //modal call here
+    console.log("winner!")
 }
 function winCon() {
     let cellOne = document.getElementById('one') 
@@ -72,14 +65,14 @@ function winCon() {
     let cellEight = document.getElementById('eight')
     let cellNine = document.getElementById('nine')
 
-    if(cellOne === cellTwo === cellThree) {winner(cellOne, cellTwo, cellThree)}
-    if(cellFour === cellFive === cellSix) {winner(cellFour, cellFive, cellSix)}
-    if(cellSeven === cellEight === cellNine) {winner(cellSeven, cellEight, cellNine)}
-    if(cellOne === cellFive === cellNine) {winner(cellOne, cellFive, cellNine)}
-    if(cellSeven === cellFive === cellThree) {winner(cellSeven, cellFive, cellThree)}
-    if(cellOne === cellFour === cellSeven) {winner(cellOne, cellFour, cellSeven)}
-    if(cellTwo === cellFive === cellEight) {winner(cellTwo, cellFive, cellEight)}
-    if(cellThree === cellSix === cellNine) {winner(cellNine, cellSix, cellThree)}
+    if(cellOne === cellTwo === cellThree) {winner(cellOne, cellTwo, cellThree)} else
+    if(cellFour === cellFive === cellSix) {winner(cellFour, cellFive, cellSix)} else
+    if(cellSeven === cellEight === cellNine) {winner(cellSeven, cellEight, cellNine)} else
+    if(cellOne === cellFive === cellNine) {winner(cellOne, cellFive, cellNine)} else
+    if(cellSeven === cellFive === cellThree) {winner(cellSeven, cellFive, cellThree)} else
+    if(cellOne === cellFour === cellSeven) {winner(cellOne, cellFour, cellSeven)} else
+    if(cellTwo === cellFive === cellEight) {winner(cellTwo, cellFive, cellEight)} else
+    if(cellThree === cellSix === cellNine) {winner(cellNine, cellSix, cellThree)} 
 }
 
 
