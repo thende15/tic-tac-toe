c2a114c6ec90f88daa35a9847bb34ea44c9bbf78
Timothy Henderson <titansmith8@hotmail.com>
2022-08-11 19:16:22 -0400

    got rid of useless function

--- starter-code/script.js
+++ starter-code/script.js
@@ -141,10 +141,6 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
