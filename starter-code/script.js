6ae67968e0f221f3037255724c6084f9c1423b4d
Timothy Henderson <titansmith8@hotmail.com>
2022-08-11 16:12:41 -0400

    adding modal

--- starter-code/script.js
+++ starter-code/script.js
@@ -88,9 +88,34 @@ function replay() {
       for (var i = 0; i < boxes.length; i++) {
         cells[i].classList.remove("win");
         cells[i].innerHTML = "";
-        turn.innerHTML = "Play";
-        turn.style.fontSize = "25px";
+        
 
       }
 
     }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
